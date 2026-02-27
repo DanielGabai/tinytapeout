f176cf60f8f62b16ffb9e4d8e69c4abd39e95b8d
Daniel Gabai <dg779@cornell.edu>
2026-02-27 01:30:18 -0500

    updated to 6 bit

--- src/lfsr.sv
+++ src/lfsr.sv
@@ -1,8 +1,8 @@
-/* 8-bit Linear Feedback Shift Register
+/* 6-bit Linear Feedback Shift Register
    Used to generate pseudo-random sequences
    Shifts to the left
-   Incoming bit is XOR of bits 8,6,5,2
-   To generate sequence, watch bits 7, 4, 1
+   Incoming bit is XOR of bits 5,4
+   To generate sequence, watch bits 5,2,0
    */
 
 module lfsr (
@@ -11,20 +11,20 @@ module lfsr (
     input logic load,
     input logic en,
 
-    input logic [7:0] seed,
+    input logic [5:0] seed,
     output logic [2:0] r_out
 );
-logic [7:0] r_store;
+logic [5:0] r_store;
 always_ff @(posedge clk) begin
     if (!rst_n) begin
-        r_store <= 8'd1;
+        r_store <= 6'd1;
     end else if (load && ~en) begin
-        r_store <= (seed == 8'd0) ? (8'd1) : seed;
+        r_store <= (seed == 6'd0) ? (6'd1) : seed;
     end else if (en) begin
-        r_store <= { r_store[6:0], (r_store[7] ^ r_store[5] ^ r_store[4] ^ r_store[1]) };
+        r_store <= { r_store[4:0], (r_store[5] ^ r_store[4]) };
     end
 end
 
-assign r_out = {r_store[6], r_store[3], r_store[0]};
+assign r_out = {r_store[5], r_store[2], r_store[0]};
 
 endmodule
