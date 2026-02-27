2e2dacc2f6f4d8e8cded15e367079e0f533b827f
Daniel Gabai <dg779@cornell.edu>
2026-02-27 15:17:17 -0500

    fixed adj not loading

--- src/delay.sv
+++ src/delay.sv
@@ -1,13 +1,3 @@
-/* Contains the delay loop used for the seven seg
-   Uses a 32-bit register to store delay
-   Technically will only count up to 2^31 + 1
-   Adjustable via input switches prior to game start
-   The input selects which bit to "watch" of counter
-   Enable must be low to load a delay value
-   */
-
-// TODO: Fix adj_delay not loading values
-
 module delay (
     input logic clk,
     input logic rst_n,
@@ -19,28 +9,33 @@ module delay (
     output logic finish
 );
 
-logic [31:0] counter;
-logic [4:0] adj_delay;
+    logic [31:0] counter;
+    logic [4:0] adj_delay;
 
-always_ff @(posedge clk) begin
-    if (!rst_n) begin
-        counter <= '0;
-        adj_delay <= '0;
-    end else begin
-        if (en) begin
-            if (!finish) begin
-                counter <= counter + 1'b1;
-            end
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            counter <= '0;
+            adj_delay <= '0;
         end else begin
+            
             if (load_delay) begin
                 adj_delay <= ui_in[4:0];
             end
+
+            // 2. Auto-resetting counter
+            if (en) begin
+                if (!finish) begin
+                    counter <= counter + 1'b1;
+                end
+            end else begin
+                counter <= '0; // Clears the timer when the FSM drops 'en'
+            end
+            
         end
     end
-end
 
-always_comb begin
-    finish = counter[adj_delay];
-end
+    always_comb begin
+        finish = counter[adj_delay];
+    end
 
 endmodule
